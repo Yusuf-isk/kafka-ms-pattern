4ac228d2ec081b7e36c469ccee0680f629fe42b3
yyusu <yyusufisik@gmail.com>
2024-03-21 13:07:02 +0300

    Initial commit

---
+++ src/test/java/com/example/kafka/kafkamspattern/KafkaMsPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka.kafkamspattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMsPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
